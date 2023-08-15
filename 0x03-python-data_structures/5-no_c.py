d689303e040297cc373ed0aba8cde6993225323c
Mamatiane <andrymamatiane@example.com>
2023-08-15 13:21:07 -0700

    0x03-python-data_structures

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
+# 5-no_c.py
+
+
 def no_c(my_string):
-    new_str = ""
-    for i in my_string:
-        if (i != 'c') and (i != 'C'):
-            new_str += i
-    return (new_str)
-
-vi 6-print_matrix_integer.py
-
-#!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        print(' '.join('{:d}'.format(j)for j in i))
+    """Remove all characters c and C from a string."""
+    copy = [x for x in my_string if x != 'c' and x != 'C']
+    return ("".join(copy))
